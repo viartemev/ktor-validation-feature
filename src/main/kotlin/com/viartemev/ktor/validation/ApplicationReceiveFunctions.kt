3f0171f49f62cab057bbafa24349e6cf2dffd194
Артемьев Вячеслав <v.artemyev@ARTEMYEV-V.orglot.office>
2019-12-24 17:14:32 +0300

    Move to body

--- src/main/kotlin/com/viartemev/ktor/validation/ApplicationReceiveFunctions.kt
+++ src/main/kotlin/com/viartemev/ktor/validation/ApplicationReceiveFunctions.kt
@@ -7,32 +7,22 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.starProjectedType
 
-suspend inline fun <reified T : Any> ApplicationCall.receiveValidated(): Pair<T, ValidationResult> {
-    val receive = receive<T>()
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend inline fun <reified T : Any> ApplicationCall.receiveValidated(): Pair<T, ValidationResult> =
+    receive<T>() to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
 
-suspend fun <T : Any> ApplicationCall.receiveValidated(type: KClass<T>): Pair<T, ValidationResult> {
-    val receive = receive<T>(type.starProjectedType)
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend fun <T : Any> ApplicationCall.receiveValidated(type: KClass<T>): Pair<T, ValidationResult> =
+    receive<T>(type.starProjectedType) to (request.call.attributes.getOrNull(ValidationFeatureKey)
+        ?: ValidationResult.NotValidated)
 
-suspend fun <T : Any> ApplicationCall.receiveValidated(type: KType): Pair<T, ValidationResult> {
-    val receive = receive<T>(type)
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend fun <T : Any> ApplicationCall.receiveValidated(type: KType): Pair<T, ValidationResult> =
+    receive<T>(type) to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
 
-suspend inline fun <reified T : Any> ApplicationCall.receiveOrNullValidated(): Pair<T?, ValidationResult> {
-    val receive = receiveOrNull<T>()
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend inline fun <reified T : Any> ApplicationCall.receiveOrNullValidated(): Pair<T?, ValidationResult> =
+    receiveOrNull<T>() to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
 
-suspend fun <T : Any> ApplicationCall.receiveOrNullValidated(type: KClass<T>): Pair<T?, ValidationResult> {
-    val receive = receiveOrNull<T>(type.starProjectedType)
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend fun <T : Any> ApplicationCall.receiveOrNullValidated(type: KClass<T>): Pair<T?, ValidationResult> =
+    receiveOrNull<T>(type.starProjectedType) to (request.call.attributes.getOrNull(ValidationFeatureKey)
+        ?: ValidationResult.NotValidated)
 
-suspend fun <T : Any> ApplicationCall.receiveOrNullValidated(type: KType): Pair<T?, ValidationResult> {
-    val receive = receiveOrNull<T>(type)
-    return receive to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
-}
+suspend fun <T : Any> ApplicationCall.receiveOrNullValidated(type: KType): Pair<T?, ValidationResult> =
+    receiveOrNull<T>(type) to (request.call.attributes.getOrNull(ValidationFeatureKey) ?: ValidationResult.NotValidated)
